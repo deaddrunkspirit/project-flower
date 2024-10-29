3a8786253043cfb557c6cac40b7175bc2c87e8ea
deaddrunkspirit <dedrsp@gmail.com>
2024-10-29 21:18:28 +0300

    project finished

--- flower/views.py
+++ flower/views.py
@@ -7,51 +7,43 @@ import datetime
 
 def list_page_view(request):
     query = request.GET.get('q', '')
-    items = Part.objects.filter(name__icontains=query) if query else Part.objects.all()
-    
-    paginator = Paginator(items, 10) 
+    search_type = request.GET.get('search_type', 'contains')
+    min_value = request.GET.get('min_value')
+    max_value = request.GET.get('max_value')
+    min_weight = request.GET.get('min_weight')
+    max_weight = request.GET.get('max_weight')
+
+    parts = Part.objects.all()
+
+    if query:
+        if search_type == 'contains':
+            parts = parts.filter(name__icontains=query)
+        elif search_type == 'starts_with':
+            parts = parts.filter(name__istartswith=query)
+
+    if min_value:
+        parts = parts.filter(value__gte=float(min_value))
+    if max_value:
+        parts = parts.filter(value__lte=float(max_value))
+
+    if min_weight:
+        parts = parts.filter(weight__gte=float(min_weight))
+    if max_weight:
+        parts = parts.filter(weight__lte=float(max_weight))
+
+    paginator = Paginator(parts, 10)
     page_number = request.GET.get('page', 1)
-    paginated_items = paginator.get_page(page_number)
-    
+    paginated_parts = paginator.get_page(page_number)
+
     context = {
         'page_title': "Parts",
-        'items': paginated_items,
-        'query': query
+        'items': paginated_parts,
+        'query': query,
+        'search_type': search_type,
+        'min_value': min_value,
+        'max_value': max_value,
+        'min_weight': min_weight,
+        'max_weight': max_weight,
     }
-    
+
     return render(request, 'page.html', context)
-
-def home(request):
-    parts = Part.objects.all()
-    parts_str = ''
-    for part in parts:
-        parts_str += get_full_part_layout(part)
-
-    layout = f'''
-        <main>
-            <h1 style="padding:30px;">Parts</h1> 
-            <input />
-            <ul style="list-style-type:none;">
-                <li style="display:flex; flex-direction:row; justify-content:space-evenly;">
-                    <p style="font-weight: bold;">Name</p>
-                    <p style="font-weight: bold;">Value</p>
-                    <p style="font-weight: bold;">Weight</p>
-                    <p style="font-weight: bold;">Created At</p>
-                </li>
-                {parts_str}
-            </ul>
-        </main>
-    '''
-
-    return HttpResponse(layout)
-    
-def get_full_part_layout(part):
-    return f'''
-        <li style="display:flex; flex-direction:row; justify-content:space-evenly;padding:30px;">
-            <div style="display:block; width:80px;">{part.name}</div>
-            <div style="display:block; width:80px;">{part.value}</div>
-            <div style="display:block; width:80px;">{part.weight}</div>
-            <div style="display:block; width:80px;">{part.created_at}</div>
-        </li>
-    '''
-
